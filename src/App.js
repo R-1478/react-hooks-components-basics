832a312ba2f547a14123635e3f9e2aa2df7c76de
evansthitai <evansnt9619@gmail.com>
2023-11-08 10:12:16 +0300

    completed lab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
-
+const nav = <Navbar>I'm a link!</Navbar>
+const home = <Home>Home!</Home> 
+const about = <About>About!</About>
 function App() {
   return (
+    <>
     <div>
+      {nav} 
+      {home}
+      {about}
       <Article />
       <Comment />
     </div>
+    </>
   );
 }
 
